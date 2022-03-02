f393583f88a783fa0d5225a8ebed48ad5e0478fd
Andrey11995 <drunya119955@yandex.ru>
2022-03-02 21:35:51 +0300

    Test workflow

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', 'postgres'),
+        'TEST': {
+            'ENGINE': 'django.db.backends.sqlite3',
+        },
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'PostgresPassword'),
         'HOST': os.getenv('DB_HOST', 'db'),
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,23 +6,23 @@ pytest_plugins = [
     'tests.fixtures.fixture_user',
     'tests.fixtures.fixture_data',
 ]
-
-
-@pytest.fixture(scope="session", autouse=True)
-def auto_session_resource(request):
-    start = time.time()
-
-    def auto_session_resource_teardown():
-        root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        path = f'{root_dir}/backend/foodgram/media/recipes/'
-        finish = time.time()
-        files = os.listdir(os.path.join(path))
-        for file in files:
-            if os.path.isfile(path + file):
-                stat = os.stat(path + file)
-                created_time = stat.st_ctime
-                if start <= created_time <= finish:
-                    os.remove(path + file)
-        print('\nТестовые файлы удалены\n')
-
-    request.addfinalizer(auto_session_resource_teardown)
+#
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def auto_session_resource(request):
+#     start = time.time()
+#
+#     def auto_session_resource_teardown():
+#         root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#         path = f'{root_dir}/backend/foodgram/media/recipes/'
+#         finish = time.time()
+#         files = os.listdir(os.path.join(path))
+#         for file in files:
+#             if os.path.isfile(path + file):
+#                 stat = os.stat(path + file)
+#                 created_time = stat.st_ctime
+#                 if start <= created_time <= finish:
+#                     os.remove(path + file)
+#         print('\nТестовые файлы удалены\n')
+#
+#     request.addfinalizer(auto_session_resource_teardown)
