17c038cb14d74def3de14fd67ae5df24c9e42415
Andrey11995 <drunya119955@yandex.ru>
2022-03-08 12:48:39 +0300

    Try to fix media, debug False

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -7,9 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns += static(settings.STATIC_URL,
-                      document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
